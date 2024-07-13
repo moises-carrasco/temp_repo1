52d684685d396fc62a4ad88661b66fa02e86cb1d
Moises Carrasco <moises.carrasco@wmcontractor.com>
2024-07-13 01:18:05 -0500

    asdfsaf

--- script01.sh
+++ script01.sh
@@ -5,10 +5,10 @@ export MYVAR1="Hello"
 export MYVAR2="World"
 
 # Imprimir las variables de entorno
-echo $MYVAR $MYVAR2
+echo $MYVAR $MYVAR1 $MYVAR2
 
 # Hacer algo con las variables de entorno
-if [ "$MYVAR" == "Hello" ] && [ "$MYVAR2" == "World" ]; then
+if [ "$MYVAR1" == "Hello" ] && [ "$MYVAR2" == "World" ]; then
     echo "Both variables are set correctly."
 else
     echo "One or both variables are not set correctly."
