d791b40660fab42d5e84265142382d2c7a95dc4c
Moises Carrasco <moises.carrasco@wmcontractor.com>
2024-07-13 01:57:11 -0500

    mklm

--- script01.sh
+++ script01.sh
@@ -5,7 +5,7 @@ export MYVAR1="Hello"
 export MYVAR2="World"
 
 # Imprimir las variables de entorno
-echo $MYVAR $MYVAR1 $MYVAR2 $otravar
+echo $MYVAR $MYVAR1 $MYVAR2 $otravar $minombre
 
 # Hacer algo con las variables de entorno
 if [ "$MYVAR1" == "Hello" ] && [ "$MYVAR2" == "World" ]; then
